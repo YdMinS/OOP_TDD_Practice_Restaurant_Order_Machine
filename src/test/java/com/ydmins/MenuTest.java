cf65d54d4a650749b422bdb9ecfc0eb0b6593e9c
ydmins <dr.ydmins@gmail.com>
2023-05-10 13:19:18 +0900

    MenuTest 수정
    - chooseTest2 : 메뉴에 없는 요리를 주문시 오류 발생 테스트 구현

--- src/test/java/com/ydmins/MenuTest.java
+++ src/test/java/com/ydmins/MenuTest.java
@@ -3,6 +3,7 @@ package com.ydmins;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.util.List;
 
@@ -13,5 +14,13 @@ public class MenuTest {
         Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
         MenuItem menuItem = menu.choose("돈까스");
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
+    }
+
+    @DisplayName("메뉴판에 없는 메뉴를 선택할 시 예외를 반환한다.")
+    @Test
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        assertThatCode(()->menu.choose("피자")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 메뉴 이름입니다.");
     }
 }
