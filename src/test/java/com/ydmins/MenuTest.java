b105146f45632fdc0c1036cfcc2479fcd3bd89c3
ydmins <dr.ydmins@gmail.com>
2023-05-10 13:08:25 +0900

    Menu, MenuTest 생성, MenuItem 수정,
    - Menu : 메뉴 생성 생성자와 메뉴선택 choose 메서드 생성
    - MenuTest : 메뉴 선택 테스트 구현
    - MenuItem : matches 메서드 구현

---
+++ src/test/java/com/ydmins/MenuTest.java
@@ -0,0 +1,17 @@
+package com.ydmins;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+public class MenuTest {
+    @DisplayName("메뉴 이름에 해당하는 메뉴를 반환한다.")
+    @Test
+    void chooseTest() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        MenuItem menuItem = menu.choose("돈까스");
+        assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
+    }
+}
